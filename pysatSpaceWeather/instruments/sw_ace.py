5dd1cd5589abbaf6dcc68a10f071580d6112b111
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2020-10-22 10:01:30 -0400

    MAINT: updated sat_id to inst_id
    Changed sat_id to inst_id.

--- pysatSpaceWeather/instruments/sw_ace.py
+++ pysatSpaceWeather/instruments/sw_ace.py
@@ -10,7 +10,7 @@ name
 tag
     - 'realtime' Real-time data from the Space Weather Prediction Center (SWPC)
     - 'historic' Historic data from the SWPC
-sat_id
+inst_id
     - 'mag' Magnetometer
     - 'swepam' Solar Wind Electron Proton Alpha Monitor
     - 'sis' Solar Isotope Spectrometer
@@ -24,7 +24,7 @@ grab today's file three times and assign dates from yesterday, today, and
 tomorrow.
 ::
 
-    mag = pysat.Instrument('sw', 'ace', sat_id='mag', tag='realtime')
+    mag = pysat.Instrument('sw', 'ace', inst_id='mag', tag='realtime')
     mag.download(start=dt.datetime.now(), stop=dt.datetime.now())
     mag.load(date=dt.datetime.now())
 
@@ -50,21 +50,21 @@ import pysatSpaceWeather.instruments.methods.ace as mm_ace
 
 logger = logging.getLogger(__name__)
 
-# Define the Instrument platform, name, tags, and sat_ids
+# Define the Instrument platform, name, tags, and inst_ids
 platform = 'sw'
 name = 'ace'
 tags = {'realtime': 'Real-time data from the SWPC',
         'historic': ' Historic data from the SWPC'}
-sat_ids = {sat_id: [tag for tag in tags.keys()]
-           for sat_id in ['mag', 'swepam', 'epam', 'sis']}
+inst_ids = {inst_id: [tag for tag in tags.keys()]
+           for inst_id in ['mag', 'swepam', 'epam', 'sis']}
 
 # Define today's date
 now = dt.datetime.now()
 
-# set test dates (first level: sat_id, second level: tag)
-_test_dates = {sat_id: {'realtime': dt.datetime(now.year, now.month, now.day),
+# set test dates (first level: inst_id, second level: tag)
+_test_dates = {inst_id: {'realtime': dt.datetime(now.year, now.month, now.day),
                         'historic': dt.datetime(2009, 1, 1)}
-               for sat_id in sat_ids.keys()}
+               for inst_id in inst_ids.keys()}
 
 
 def init(self):
@@ -76,14 +76,14 @@ def init(self):
 
     # Set the appropraite acknowledgements and references
     self.acknowledgements = mm_ace.acknowledgements(self.platform)
-    self.references = mm_ace.references(self.sat_id)
+    self.references = mm_ace.references(self.inst_id)
 
     logger.info(self.acknowledgements)
 
     return
 
 
-def load(fnames, tag=None, sat_id=None):
+def load(fnames, tag=None, inst_id=None):
     """Load the ACE space weather prediction data
 
     Parameters
@@ -92,7 +92,7 @@ def load(fnames, tag=None, sat_id=None):
         Series, list, or array of filenames
     tag : str or NoneType
         tag or None (default=None)
-    sat_id : str or NoneType
+    inst_id : str or NoneType
         ACE instrument or None (default=None)
 
     Returns
@@ -105,7 +105,7 @@ def load(fnames, tag=None, sat_id=None):
     Raises
     ------
     ValueError
-        When unknown sat_id is supplied.
+        When unknown inst_id is supplied.
 
     Note
     ----
@@ -129,7 +129,7 @@ def load(fnames, tag=None, sat_id=None):
                    meta.desc_label: 'Seconds of Julian Day'}
     status_desc = '0 = nominal data, 1 to 8 = bad data record, 9 = no data'
 
-    if sat_id == 'mag':
+    if inst_id == 'mag':
         meta['status'] = {meta.name_label: 'Status',
                           meta.desc_label: status_desc}
         meta['bx_gsm'] = {meta.units_label: 'nT',
@@ -158,7 +158,7 @@ def load(fnames, tag=None, sat_id=None):
                            meta.desc_label: 'GSM Longitude',
                            meta.fill_label: -999.9,
                            meta.min_label: 0.0, meta.max_label: 360.0}
-    elif sat_id == 'epam':
+    elif inst_id == 'epam':
         flux_desc = '5-min averaged Differential '
         meta['status_e'] = {meta.name_label: 'Diff e- Flux Status',
                             meta.desc_label: status_desc}
@@ -215,7 +215,7 @@ def load(fnames, tag=None, sat_id=None):
                                             'Proton Flux between 1060-1900',
                                             ' keV']),
                                    meta.fill_label: -1.0e5}
-    elif sat_id == "swepam":
+    elif inst_id == "swepam":
         sw_desc = '1-min averaged Solar Wind '
         meta['status'] = {meta.name_label: 'Status',
                           meta.desc_label: status_desc}
@@ -234,7 +234,7 @@ def load(fnames, tag=None, sat_id=None):
                                meta.desc_label: ''.join([sw_desc,
                                                          'Ion Temperature']),
                                meta.fill_label: -1.0e5}
-    elif sat_id == 'sis':
+    elif inst_id == 'sis':
         flux_name = 'Integral Proton Flux'
         meta['status_10'] = {meta.name_label: ''.join([flux_name,
                                                        ' > 10 MeV Status']),
@@ -257,12 +257,12 @@ def load(fnames, tag=None, sat_id=None):
                                                              ' > 30 MeV']),
                                    meta.fill_label: -1.0e5}
     else:
-        raise ValueError('unknown sat_id {:}'.format(sat_id))
+        raise ValueError('unknown inst_id {:}'.format(inst_id))
 
     return data, meta
 
 
-def list_files(tag=None, sat_id=None, data_path=None, format_str=None):
+def list_files(tag=None, inst_id=None, data_path=None, format_str=None):
     """Return a Pandas Series of every file for ACE data
 
     Parameters
@@ -270,7 +270,7 @@ def list_files(tag=None, sat_id=None, data_path=None, format_str=None):
     tag : string or NoneType
         Denotes type of file to load.
         (default=None)
-    sat_id : string or NoneType
+    inst_id : string or NoneType
         Specifies the satellite ID for a constellation.  Not used.
         (default=None)
     data_path : string or NoneType
@@ -291,7 +291,7 @@ def list_files(tag=None, sat_id=None, data_path=None, format_str=None):
 
     """
 
-    format_str = '_'.join(["ace", sat_id, tag,
+    format_str = '_'.join(["ace", inst_id, tag,
                            '{year:04d}-{month:02d}-{day:02d}.txt'])
     if data_path is not None:
         files = pysat.Files.from_os(data_path=data_path, format_str=format_str)
@@ -302,7 +302,7 @@ def list_files(tag=None, sat_id=None, data_path=None, format_str=None):
     return files
 
 
-def download(date_array, tag, sat_id, data_path):
+def download(date_array, tag, inst_id, data_path):
     """Routine to download ACE Space Weather data
 
     Parameters
@@ -312,7 +312,7 @@ def download(date_array, tag, sat_id, data_path):
     tag : string or NoneType
         Denotes type of file to load. Accepted types are 'realtime' and
         'historic' (default=None)
-    sat_id : string or NoneType
+    inst_id : string or NoneType
         Specifies the ACE instrument. Accepts 'mag', 'sis', 'epam', 'swepam'
     data_path : string or NoneType
         Path to data directory. If None is specified, the value previously
@@ -332,14 +332,14 @@ def download(date_array, tag, sat_id, data_path):
     # date range
     if tag == 'realtime':
         file_fmt = "{:s}-{:s}.txt".format("ace", "magnetometer"
-                                          if sat_id == "mag" else sat_id)
+                                          if inst_id == "mag" else inst_id)
 
         if(len(date_array) > 1 or date_array[0].year != now.year or
            date_array[0].month != now.month or date_array[0] != now.day):
             logging.warning('real-time data only available for current day')
     else:
-        data_rate = 1 if sat_id in ['mag', 'swepam'] else 5
-        file_fmt = '_'.join(["%Y%m%d", "ace", sat_id,
+        data_rate = 1 if inst_id in ['mag', 'swepam'] else 5
+        file_fmt = '_'.join(["%Y%m%d", "ace", inst_id,
                              '{:d}m.txt'.format(data_rate)])
 
     url = {'realtime': 'https://services.swpc.noaa.gov/text/',
@@ -367,15 +367,15 @@ def download(date_array, tag, sat_id, data_path):
         raw_data = raw_data.split('\n')[1:]  # Remove the last header line
 
         # Parse the file, treating the 4 time columns separately
-        data_dict = {col: list() for col in data_cols[sat_id]}
+        data_dict = {col: list() for col in data_cols[inst_id]}
         times = list()
-        nsplit = len(data_cols[sat_id]) + 4
+        nsplit = len(data_cols[inst_id]) + 4
         for raw_line in raw_data:
             split_line = raw_line.split()
             if len(split_line) == nsplit:
                 times.append(dt.datetime.strptime(' '.join(split_line[:4]),
                                                   '%Y %m %d %H%M'))
-                for i, col in enumerate(data_cols[sat_id]):
+                for i, col in enumerate(data_cols[inst_id]):
                     # Convert to a number and save
                     #
                     # Output is saved as a float, so don't bother to
@@ -390,7 +390,7 @@ def download(date_array, tag, sat_id, data_path):
         data = pds.DataFrame(data_dict, index=times)
 
         # write out as a file
-        data_file = '{:s}.txt'.format('_'.join(["ace", sat_id, tag,
+        data_file = '{:s}.txt'.format('_'.join(["ace", inst_id, tag,
                                                 dl_date.strftime('%Y-%m-%d')]))
         data.to_csv(os.path.join(data_path, data_file), header=True)
 
@@ -426,10 +426,10 @@ def clean(self):
             self.data[col] = self.data[col].replace(fill_val, np.nan)
 
     # Replace bad values with NaN and remove times with no valid data
-    if self.sat_id in ['mag', 'swepam']:
+    if self.inst_id in ['mag', 'swepam']:
         self.data = self.data[self.data['status'] <= max_status]
     else:
-        if self.sat_id == 'epam':
+        if self.inst_id == 'epam':
             ecols = ['eflux_38-53', 'eflux_175-315']
             # Evaluate the electron flux data
             for col in ecols:
@@ -444,7 +444,7 @@ def clean(self):
             # Include both fluxes and the anisotropy index in the removal eval
             eval_cols = ecols + pcols
             eval_cols.append('anis_ind')
-        elif self.sat_id == 'sis':
+        elif self.inst_id == 'sis':
             # Evaluate the different proton fluxes
             self.data['int_pflux_10MeV'][
                 self.data['status_10'] > max_status] = np.nan
@@ -453,7 +453,7 @@ def clean(self):
 
             eval_cols = ['int_pflux_10MeV', 'int_pflux_30MeV']
         else:
-            raise ValueError('unknown sat_id {:}'.format(self.sat_id))
+            raise ValueError('unknown inst_id {:}'.format(self.inst_id))
 
         # Remove lines without any good data
         good_cols = (~np.isnan(self.data.loc[:, eval_cols])).sum(axis=1)

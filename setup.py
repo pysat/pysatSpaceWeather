6f44c5787b544c86426d170026b247add76818c5
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2020-11-18 11:50:01 -0500

    MAINT: block comments about cfg
    Added comments about use of setup.cfg.

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Copyright (C) 2020, Authors
-# Full license can be found in License.md and AUTHORS.md
+# Full license can be found in License.md
 # -----------------------------------------------------------------------------
 
 from setuptools import setup
 
 
-# Run setup
+# Run setup.  Setuptools will look for parameters in [metadata] section of
+# setup.cfg
 setup()

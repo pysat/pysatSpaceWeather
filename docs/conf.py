1fb13eb077ba2f8cff187453a88af698eb00d233
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2020-11-19 14:55:58 -0500

    BUG: m2r is broken
    The `m2r` and `m2r2` modules are both broken for python3, but we can no longer compile using python2.  `m2r2` is close to having a fix, which works locally.

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.viewcode',
               'sphinx.ext.githubpages',
               'numpydoc',
-              'm2r',
+              'm2r2'
 ]
 
 # Add any paths that contain templates here, relative to this directory.

077ba064814d13eafde331d19f7f55d7f5e3c7a3
Angeline Burrell <aburrell@users.noreply.github.com>
2021-05-20 09:01:01 -0400

    BUG: fixed float check
    Fixed float check to identify both numpy floats and normal floats.

--- pysatSpaceWeather/instruments/methods/general.py
+++ pysatSpaceWeather/instruments/methods/general.py
@@ -19,7 +19,7 @@ def preprocess(inst):
     for col in inst.data.columns:
         fill_val = inst.meta[col][inst.meta.labels.fill_val]
         # Ensure we are dealing with a float for future nan comparison
-        if isinstance(fill_val, np.floating):
+        if isinstance(fill_val, np.floating) or isinstance(fill_val, float):
             if ~np.isnan(fill_val):
                 inst.data[col] = inst.data[col].replace(fill_val, np.nan)
                 inst.meta[col] = {inst.meta.labels.fill_val: np.nan}

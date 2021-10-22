7038ca2a929975cad846bdcda636c4f818dc97fc
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2021-10-22 17:00:47 -0400

    TST: added unit tests
    Added unit tests for the new ACE SWEPAM method.

--- pysatSpaceWeather/tests/test_methods_ace.py
+++ pysatSpaceWeather/tests/test_methods_ace.py
@@ -5,6 +5,7 @@
 # ----------------------------------------------------------------------------
 """Integration and unit test suite for ACE methods."""
 
+import pysat
 import pytest
 
 from pysatSpaceWeather.instruments.methods import ace as mm_ace
@@ -42,4 +43,47 @@ class TestACEMethods(object):
             mm_ace.references('ace')
 
         assert str(kerr.value).find('unknown ACE instrument') >= 0
+        return
+
+
+class TestACESWEPAMMethods(object):
+    """Test class for ACE SWEPAM methods."""
+
+    def setup(self):
+        """Create a clean testing setup."""
+        self.testInst = pysat.Instrument('pysat', 'testing')
+        self.testInst.load(date=self.testInst.inst_module._test_dates[''][''])
+
+        self.omni_keys = ['sw_proton_dens_norm', 'sw_ion_temp_norm']
+        return
+
+    def teardown(self):
+        """Clean up previous testing setup."""
+        del self.testInst
+        return
+
+    def test_ace_swepam_hourly_omni_norm(self):
+        """Test ACE SWEPAM conversion to OMNI hourly normalized standards."""
+
+        self.testInst['slt'] *= 400.0
+        mm_ace.ace_swepam_hourly_omni_norm(self.testInst, speed_key='slt',
+                                           dens_key='mlt', temp_key='dummy3')
+
+        # Test that normalized data and metadata are present and realistic
+        for okey in self.omni_keys:
+            assert okey in self.testInst.variables
+            assert okey in self.testInst.meta.keys()
+            assert (self.testInst[okey].values >= 0.0).all()
+
+        return
+
+    def test_ace_swepam_hourly_omni_norm_bad_keys(self):
+        """Test ACE SWEPAM conversion to OMNI hourly normalized standards."""
+
+        with pytest.raises(ValueError) as verr:
+            mm_ace.ace_swepam_hourly_omni_norm(self.testInst)
+
+        # Test the error message for missing data variables
+        assert str(verr).find("instrument missing variable") >= 0
+
         return

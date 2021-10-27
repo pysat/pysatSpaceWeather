fb90025fc68e3e60d0db02728dc5e9bf4351b8ff
Angeline G. Burrell <angeline.burrell@nrl.navy.mil>
2021-10-27 12:00:34 -0400

    ENH: added LASP predicted Dst
    Added a new data set, the LASP predicted Dst.

--- pysatSpaceWeather/instruments/sw_dst.py
+++ pysatSpaceWeather/instruments/sw_dst.py
@@ -9,6 +9,7 @@ name
     'dst'
 tag
     'noaa' - Historic Dst data coalated by and maintained by NOAA/NCEI
+    'lasp' - Predicted Dst from real-time ACE or DSCOVR provided by LASP
 inst_id
     ''
 
@@ -30,6 +31,7 @@ import ftplib
 import numpy as np
 import os
 import pandas as pds
+import requests
 
 import pysat
 
@@ -42,13 +44,19 @@ logger = pysat.logger
 
 platform = 'sw'
 name = 'dst'
-tags = {'noaa': 'Historic Dst data coalated by and maintained by NOAA/NCEI'}
-inst_ids = {'': [tag for tag in tags]}
+tags = {'noaa': 'Historic Dst data coalated by and maintained by NOAA/NCEI',
+        'lasp': 'Predicted Dst from real-time ACE or DSCOVR provided by LASP'}
+inst_ids = {'': [tag for tag in tags.keys()]}
+
+# Generate today's date to support loading predicted data sets
+now = dt.datetime.utcnow()
+today = dt.datetime(now.year, now.month, now.day)
+tomorrow = today + dt.timedelta(days=1)
 
 # ----------------------------------------------------------------------------
 # Instrument test attributes
 
-_test_dates = {'': {'noaa': dt.datetime(2007, 1, 1)}}
+_test_dates = {'': {'noaa': dt.datetime(2007, 1, 1), 'lasp': today}}
 
 # Other tags assumed to be True
 _test_download_travis = {'': {'noaa': False}}
@@ -103,70 +111,74 @@ def load(fnames, tag, inst_id):
 
     all_data = []
 
-    # Dst data is actually stored by year but users can load by day.
-    # Extract the actual dates from the input list of filenames as
-    # well as the names of the actual files.
-    fdates = []
-    ufnames = []
-    for filename in fnames:
-        fdates.append(dt.datetime.strptime(filename[-10:], '%Y-%m-%d'))
-        ufnames.append(filename[0:-11])
+    if tag == 'noaa':
+        # NOAA Dst data is actually stored by year, but users can load by day.
+        # Extract the actual dates from the input list of filenames as well as
+        # the names of the actual files.
+        fdates = []
+        ufnames = []
+        for filename in fnames:
+            fdates.append(dt.datetime.strptime(filename[-10:], '%Y-%m-%d'))
+            ufnames.append(filename[0:-11])
 
-    # Get unique filenames that map to actual data
-    ufnames = np.unique(ufnames).tolist()
+            # Get unique filenames that map to actual data
+            ufnames = np.unique(ufnames).tolist()
 
-    # Load unique files
-    for fname in ufnames:
-        with open(fname) as open_f:
-            lines = open_f.readlines()
-            idx = 0
+        # Load unique files
+        for fname in ufnames:
+            with open(fname) as open_f:
+                lines = open_f.readlines()
+                idx = 0
 
-            # Check if all lines are good
-            max_lines = 0
-            for line in lines:
-                if len(line) > 1:
-                    max_lines += 1
+                # Check if all lines are good
+                max_lines = 0
+                for line in lines:
+                    if len(line) > 1:
+                        max_lines += 1
 
-            # Prep memory
-            yr = np.zeros(max_lines * 24, dtype=int)
-            mo = np.zeros(max_lines * 24, dtype=int)
-            day = np.zeros(max_lines * 24, dtype=int)
-            ut = np.zeros(max_lines * 24, dtype=int)
-            dst = np.zeros(max_lines * 24, dtype=int)
+                # Prep memory
+                yr = np.zeros(max_lines * 24, dtype=int)
+                mo = np.zeros(max_lines * 24, dtype=int)
+                day = np.zeros(max_lines * 24, dtype=int)
+                ut = np.zeros(max_lines * 24, dtype=int)
+                dst = np.zeros(max_lines * 24, dtype=int)
 
-            # Read data
-            for line in lines:
-                if len(line) > 1:
-                    temp_year = int(line[14:16] + line[3:5])
-                    if temp_year > 57:
-                        temp_year += 1900
-                    else:
-                        temp_year += 2000
+                # Read data
+                for line in lines:
+                    if len(line) > 1:
+                        temp_year = int(line[14:16] + line[3:5])
+                        if temp_year > 57:
+                            temp_year += 1900
+                        else:
+                            temp_year += 2000
 
-                    yr[idx:idx + 24] = temp_year
-                    mo[idx:idx + 24] = int(line[5:7])
-                    day[idx:idx + 24] = int(line[8:10])
-                    ut[idx:idx + 24] = np.arange(24)
-                    temp = line.strip()[20:-4]
-                    temp2 = [temp[4 * i:4 * (i + 1)] for i in np.arange(24)]
-                    dst[idx:idx + 24] = temp2
-                    idx += 24
+                        yr[idx:idx + 24] = temp_year
+                        mo[idx:idx + 24] = int(line[5:7])
+                        day[idx:idx + 24] = int(line[8:10])
+                        ut[idx:idx + 24] = np.arange(24)
+                        temp = line.strip()[20:-4]
+                        temp2 = [temp[4 * i:4 * (i + 1)] for i in np.arange(24)]
+                        dst[idx:idx + 24] = temp2
+                        idx += 24
 
-            # Prep datetime index for the data and create DataFrame
-            start = dt.datetime(yr[0], mo[0], day[0], ut[0])
-            stop = dt.datetime(yr[-1], mo[-1], day[-1], ut[-1])
-            dates = pds.date_range(start, stop, freq='H')
-            new_data = pds.DataFrame(dst, index=dates, columns=['dst'])
+                # Prep datetime index for the data and create DataFrame
+                start = dt.datetime(yr[0], mo[0], day[0], ut[0])
+                stop = dt.datetime(yr[-1], mo[-1], day[-1], ut[-1])
+                dates = pds.date_range(start, stop, freq='H')
+                new_data = pds.DataFrame(dst, index=dates, columns=['dst'])
 
-            # Add to all data loaded for filenames
-            all_data.append(new_data)
+                # Add to all data loaded for filenames
+                all_data.append(new_data)
 
-    # Combine data together
-    data = pds.concat(all_data, sort=True, axis=0)
+        # Combine data together
+        data = pds.concat(all_data, sort=True, axis=0)
 
-    # Pull out requested days
-    data = data.iloc[data.index >= fdates[0], :]
-    data = data.iloc[data.index < fdates[-1] + pds.DateOffset(days=1), :]
+        # Pull out requested days
+        data = data.iloc[data.index >= fdates[0], :]
+        data = data.iloc[data.index < fdates[-1] + pds.DateOffset(days=1), :]
+    else:
+        data = pysat.instruments.methods.general.load_csv_data(
+            fnames, read_csv_kwargs={'index_col': 0, 'parse_dates': True})
 
     # Create metadata
     meta = pysat.Meta()
@@ -198,7 +210,7 @@ def list_files(tag, inst_id, data_path, format_str=None):
 
     Returns
     -------
-    pysat.Files.from_os : pysat._files.Files
+    files : pysat.utils.files.Files
         A class containing the verified available files
 
     Raises
@@ -211,29 +223,32 @@ def list_files(tag, inst_id, data_path, format_str=None):
     Called by pysat. Not intended for direct use by user.
 
     """
-
-    if data_path is not None:
+    # Get the format string, if not supplied by the user
+    if format_str is None:
         if tag == 'noaa':
-            # files are by year, going to add date to yearly filename for
-            # each day of the month. The load routine will load a month of
-            # data and use the appended date to select out appropriate data.
-            if format_str is None:
-                format_str = 'dst{year:4d}.txt'
-            out = pysat.Files.from_os(data_path=data_path,
-                                      format_str=format_str)
-            if not out.empty:
-                out.loc[out.index[-1] + pds.DateOffset(years=1)
-                        - pds.DateOffset(days=1)] = out.iloc[-1]
-                out = out.asfreq('D', 'pad')
-                out = out + '_' + out.index.strftime('%Y-%m-%d')
-            return out
+            format_str = 'dst{year:4d}.txt'
         else:
-            raise ValueError(''.join(('Unrecognized tag name for Space ',
-                                      'Weather Dst Index')))
+            format_str = ''.join(['sw_dst_', tag, '_{year:4d}-{month:2d}-',
+                                  '{day:2d}.txt'])
+
+    # Get the desired files
+    if data_path is not None:
+        files = pysat.Files.from_os(data_path=data_path, format_str=format_str)
+
+        if tag == 'noaa':
+            # NOAA files yearly, so we need to add daily dates to the yearly
+            # filenames. The load routine will load a month of data and use
+            # the appended date to select out appropriate data.
+            if not files.empty:
+                files.loc[files.index[-1] + pds.DateOffset(years=1)
+                          - pds.DateOffset(days=1)] = files.iloc[-1]
+                files = files.asfreq('D', 'pad')
+                files = files + '_' + files.index.strftime('%Y-%m-%d')
     else:
         raise ValueError(''.join(('A data_path must be passed to the loading ',
                                   'routine for Dst')))
-    return
+
+    return files
 
 
 def download(date_array, tag, inst_id, data_path):
@@ -255,31 +270,73 @@ def download(date_array, tag, inst_id, data_path):
     Called by pysat. Not intended for direct use by user.
 
     """
-    # Connect to host, default port
-    ftp = ftplib.FTP('ftp.ngdc.noaa.gov')
+    if tag == 'noaa':
+        # Connect to host, default port
+        ftp = ftplib.FTP('ftp.ngdc.noaa.gov')
 
-    # User anonymous, passwd anonymous@
-    ftp.login()
-    ftp.cwd('/STP/GEOMAGNETIC_DATA/INDICES/DST')
+        # User anonymous, passwd anonymous@
+        ftp.login()
+        ftp.cwd('/STP/GEOMAGNETIC_DATA/INDICES/DST')
 
-    # Data stored by year. Only download for unique set of input years.
-    years = np.array([date.year for date in date_array])
-    years = np.unique(years)
-    for year in years:
-        fname_root = 'dst{year:04d}.txt'
-        fname = fname_root.format(year=year)
-        saved_fname = os.path.join(data_path, fname)
-        try:
-            logger.info('Downloading file for {year:04d}'.format(year=year))
-            with open(saved_fname, 'wb') as fp:
-                ftp.retrbinary('RETR ' + fname, fp.write)
-        except ftplib.error_perm as exception:
-            if str(exception.args[0]).split(" ", 1)[0] != '550':
-                raise
-            else:
-                # file not present
-                os.remove(saved_fname)
-                logger.info('File not available for {:04d}'.format(year))
+        # Data stored by year. Only download for unique set of input years.
+        years = np.array([date.year for date in date_array])
+        years = np.unique(years)
+        for year in years:
+            fname_root = 'dst{year:04d}.txt'
+            fname = fname_root.format(year=year)
+            saved_fname = os.path.join(data_path, fname)
+            try:
+                logger.info('Downloading file for {year:04d}'.format(year=year))
+                with open(saved_fname, 'wb') as fp:
+                    ftp.retrbinary('RETR ' + fname, fp.write)
+            except ftplib.error_perm as exception:
+                if str(exception.args[0]).split(" ", 1)[0] != '550':
+                    raise
+                else:
+                    # File not present
+                    os.remove(saved_fname)
+                    logger.info('File not available for {:04d}'.format(year))
 
-    ftp.close()
+        ftp.close()
+    elif tag == 'lasp':
+        # Set the remote data variables
+        url = ''.join(['https://lasp.colorado.edu/space_weather/dsttemerin/',
+                       'dst_last_96_hrs.txt'])
+        data_dict = {'time': [], 'dst': []}
+
+        # Download the webpage
+        req = requests.get(url)
+
+        # Test to see if the file was found on the server
+        if req.text.find('not found on this server') > 0:
+            logger.warning(''.join(['LASP last 96 hour Dst file not found on ',
+                                    'server: ', url]))
+        else:
+            # Split the file into lines, removing the header and
+            # trailing empty line
+            file_lines = req.text.split('\n')[1:-1]
+
+            # Format the data
+            for line in file_lines:
+                # Split the line on whitespace
+                line_cols = line.split()
+
+                if len(line_cols) != 2:
+                    raise IOError(''.join(['unexpected line encountered in ',
+                                           'file retrieved from ', url, ':\n',
+                                           line]))
+
+                # Format the time and Dst values
+                data_dict['time'].append(
+                    dt.datetime.strptime(line_cols[0], '%Y/%j-%H:%M:%S'))
+                data_dict['dst'].append(float(line_cols[1]))
+
+        # Re-cast the data as a pandas DataFrame
+        data = pds.DataFrame(data_dict, index=data_dict['time'])
+
+        # Write out as a file
+        file_base = '_'.join(['sw', 'dst', tag, today.strftime('%Y-%m-%d')])
+        file_name = os.path.join(data_path, '{:s}.txt'.format(file_base))
+        data.to_csv(file_name)
+
     return

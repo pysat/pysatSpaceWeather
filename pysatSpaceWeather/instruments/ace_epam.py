6468691a0c8dad2925f36106eb95f2a3999ead2e
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2022-05-27 08:42:39 -0400

    STY: updated defaults
    Updated function and method defaults to conform to pysat standards.

--- pysatSpaceWeather/instruments/ace_epam.py
+++ pysatSpaceWeather/instruments/ace_epam.py
@@ -131,7 +131,7 @@ download = functools.partial(mm_ace.download, name=name, now=now)
 list_files = functools.partial(mm_ace.list_files, name=name)
 
 
-def load(fnames, tag, inst_id):
+def load(fnames, tag='', inst_id=''):
     """Load the ACE space weather prediction data.
 
     Parameters
@@ -139,9 +139,9 @@ def load(fnames, tag, inst_id):
     fnames : array-like
         Series, list, or array of filenames.
     tag : str
-        Instrument tag, not used.
+        Instrument tag (default='').
     inst_id : str
-        ACE instrument ID, not used.
+        ACE instrument ID (default='').
 
     Returns
     -------

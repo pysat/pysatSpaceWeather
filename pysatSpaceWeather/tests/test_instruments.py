24ea2468a2d5c16f0c2032c1617f0fbc70436700
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2022-11-17 00:57:40 -0500

    TST: added test for deprecations
    Added tests for the old GFZ tag deprecation warning.

--- pysatSpaceWeather/tests/test_instruments.py
+++ pysatSpaceWeather/tests/test_instruments.py
@@ -6,11 +6,16 @@ Imports test methods from pysat.tests.instrument_test_class
 
 """
 
+import datetime as dt
+import warnings
+
 # Make sure to import your instrument library here
 import pysatSpaceWeather
 
 # Import the test classes from pysat
+import pysat
 from pysat.tests.classes import cls_instrument_library as clslib
+from pysat.utils import testing
 
 
 # Tell the standard tests which instruments to run each test on.
@@ -28,3 +33,48 @@ class TestInstruments(clslib.InstLibTests):
     instrument test class.
 
     """
+
+
+class TestLocalDeprecation(object):
+    """Unit tests for local instrument deprecation warnings."""
+
+    def setup_method(self):
+        """Set up the unit test environment for each method."""
+
+        warnings.simplefilter("always", DeprecationWarning)
+        self.in_kwargs = {"platform": 'sw', "name": 'kp', 'tag': ''}
+        self.ref_time = dt.datetime(2001, 1, 1)
+        self.warn_msgs = []
+        self.war = ""
+        return
+
+    def teardown_method(self):
+        """Clean up the unit test environment after each method."""
+
+        del self.in_kwargs, self.ref_time, self.warn_msgs, self.war
+        return
+
+    def eval_warnings(self):
+        """Evaluate the number and message of the raised warnings."""
+
+        # Ensure the minimum number of warnings were raised.
+        assert len(self.war) >= len(self.warn_msgs)
+
+        # Test the warning messages, ensuring each attribute is present.
+        testing.eval_warnings(self.war, self.warn_msgs)
+        return
+
+    def test_sw_kp_default_tag_deprecation(self):
+        """Test the deprecation of the '' tag for the sw_kp Instrument."""
+
+        with warnings.catch_warnings(record=True) as self.war:
+            pysat.Instrument(**self.in_kwargs)
+
+        self.warn_msgs = ["".join(["Changes at the GFZ database have led to ",
+                                   "this data type being deprecated. Switch ",
+                                   "to using 'def' for definitive Kp or ",
+                                   "'now' for Kp nowcasts from GFZ."])]
+
+        # Evaluate the warning output
+        self.eval_warnings()
+        return

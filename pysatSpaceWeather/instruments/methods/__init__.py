1fa719fa11264ac4fe014bffeb761ecb5e15572e
Angeline G. Burrell <angeline.burrell@nrl.navy.mil>
2022-12-02 13:24:41 -0500

    ENH: added general LISIRD functions
    Added general functions to support LISIRD downloads and data sets.

--- pysatSpaceWeather/instruments/methods/__init__.py
+++ pysatSpaceWeather/instruments/methods/__init__.py
@@ -1,3 +1,4 @@
 from pysatSpaceWeather.instruments.methods import general  # noqa F401
 from pysatSpaceWeather.instruments.methods import ace, dst  # noqa F401
 from pysatSpaceWeather.instruments.methods import f107, kp_ap  # noqa F401
+from pysatSpaceWeather.instruments.methods import lisird # noqa F401

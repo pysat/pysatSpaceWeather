ab294c850692f1abd7591ff188968f0ca663adf9
Angeline G. Burrell <angeline.burrell@nrl.navy.mil>
2022-12-02 13:45:07 -0500

    BUG: added missing imports
    Added missing imports for the new sub-modules.

--- pysatSpaceWeather/instruments/methods/__init__.py
+++ pysatSpaceWeather/instruments/methods/__init__.py
@@ -1,3 +1,7 @@
+from pysatSpaceWeather.instruments.methods import ace  # noqa F401
+from pysatSpaceWeather.instruments.methods import dst  # noqa F401
+from pysatSpaceWeather.instruments.methods import f107  # noqa F401
 from pysatSpaceWeather.instruments.methods import general  # noqa F401
-from pysatSpaceWeather.instruments.methods import ace, dst  # noqa F401
-from pysatSpaceWeather.instruments.methods import f107, kp_ap  # noqa F401
+from pysatSpaceWeather.instruments.methods import gfz  # noqa F401
+from pysatSpaceWeather.instruments.methods import kp_ap  # noqa F401
+from pysatSpaceWeather.instruments.methods import swpc  # noqa F401

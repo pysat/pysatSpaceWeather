b2dcf6d1869272f4e81d9520d0b4f40c1b988457
Angeline G. Burrell <angeline.burrell@nrl.navy.mil>
2023-10-19 16:17:34 -0400

    BUG: added missing variable
    Re-added a variable that was accidentally removed.

--- pysatSpaceWeather/__init__.py
+++ pysatSpaceWeather/__init__.py
@@ -2,9 +2,13 @@
 
 try:
     from importlib import metadata
+    from importlib import resources
 except ImportError:
     import importlib_metadata as metadata
+    import importlib-resources as resources
 
 from pysatSpaceWeather import instruments  # noqa F401
 
 __version__ = metadata.version('pysatSpaceWeather')
+test_data_path = str(resources.path(__package__, 'tests',
+                                    'test_data').__enter__())

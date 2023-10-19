bd2e056d1b372850ad34b94c57d4862cc793925c
Angeline G. Burrell <angeline.burrell@nrl.navy.mil>
2023-10-19 16:36:10 -0400

    BUG: fixed path call
    Fixed the path call to use the resource library correctly.

--- pysatSpaceWeather/__init__.py
+++ pysatSpaceWeather/__init__.py
@@ -11,5 +11,6 @@ from os import path
 from pysatSpaceWeather import instruments  # noqa F401
 
 __version__ = metadata.version('pysatSpaceWeather')
-test_data_path = str(
-    resources.path(__package__, path.join('tests', 'test_data')).__enter__())
+test_data_path = path.join(
+    path.split(str(resources.path(__package__, '__init__.py').__enter__()))[0],
+    'tests', 'test_data')

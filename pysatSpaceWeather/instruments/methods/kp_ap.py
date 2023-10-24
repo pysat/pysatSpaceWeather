257b656dadaaf38a208f16574405135be50d8410
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2023-10-24 12:27:48 -0400

    MAINT: updated load kwargs and try/except
    Updated the load kwargs and the temporary try/except loop to reflect changes in pysat.

--- pysatSpaceWeather/instruments/methods/kp_ap.py
+++ pysatSpaceWeather/instruments/methods/kp_ap.py
@@ -368,15 +368,14 @@ def filter_geomag(inst, min_kp=0, max_kp=9, filter_time=24, kp_inst=None,
     if kp_inst.empty:
         load_kwargs = {'date': inst.index[0], 'end_date': inst.index[-1],
                        'verifyPad': True}
-        if Version(pysat.__version__) > Version('3.0.1'):
+
+        # TODO(#131): Remove version check after minimum version supported
+        # is 3.2.0
+        if all([Version(pysat.__version__) > Version('3.0.1'),
+                Version(pysat.__version__) < Version('3.2.0')]):
             load_kwargs['use_header'] = True
 
-        # TODO(#117): This gets around a bug in pysat that will be fixed in
-        # pysat version 3.1.0+
-        try:
-            kp_inst.load(**load_kwargs)
-        except TypeError:
-            pass
+        kp_inst.load(**load_kwargs)
 
     if kp_inst.empty:
         raise IOError(
@@ -598,7 +597,11 @@ def combine_kp(standard_inst=None, recent_inst=None, forecast_inst=None,
         # Load and save the standard data for as many times as possible
         if inst_flag == 'standard':
             load_kwargs = {'date': itime}
-            if Version(pysat.__version__) > Version('3.0.1'):
+
+            # TODO(#131): Remove version check after minimum version supported
+            # is 3.2.0
+            if all([Version(pysat.__version__) > Version('3.0.1'),
+                    Version(pysat.__version__) < Version('3.2.0')]):
                 load_kwargs['use_header'] = True
 
             standard_inst.load(**load_kwargs)
@@ -628,7 +631,11 @@ def combine_kp(standard_inst=None, recent_inst=None, forecast_inst=None,
             for filename in files:
                 if filename is not None:
                     load_kwargs = {'fname': filename}
-                    if Version(pysat.__version__) > Version('3.0.1'):
+
+                    # TODO(#131): Remove version check after minimum version
+                    # supported is 3.2.0
+                    if all([Version(pysat.__version__) > Version('3.0.1'),
+                            Version(pysat.__version__) < Version('3.2.0')]):
                         load_kwargs['use_header'] = True
                     recent_inst.load(**load_kwargs)
 
@@ -664,7 +671,11 @@ def combine_kp(standard_inst=None, recent_inst=None, forecast_inst=None,
             for filename in files:
                 if filename is not None:
                     load_kwargs = {'fname': filename}
-                    if Version(pysat.__version__) > Version('3.0.1'):
+
+                    # TODO(#131): Remove version check after minimum version
+                    # supported is 3.2.0
+                    if all([Version(pysat.__version__) > Version('3.0.1'),
+                            Version(pysat.__version__) < Version('3.2.0')]):
                         load_kwargs['use_header'] = True
                     forecast_inst.load(**load_kwargs)
 

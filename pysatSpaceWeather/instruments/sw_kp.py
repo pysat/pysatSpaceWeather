1fa103dc3565ac2c3ecefd8e3601d4f7524b371c
Angeline G. Burrell <angeline.burrell@nrl.navy.mil>
2024-08-14 17:05:58 -0400

    DEP: removed support for deprecated tag, ''
    Removed support for the deprecated GFZ dataset with tag ''.

--- pysatSpaceWeather/instruments/sw_kp.py
+++ pysatSpaceWeather/instruments/sw_kp.py
@@ -16,7 +16,6 @@ platform
 name
     'kp'
 tag
-    - '' Deprecated, mixed definitive and nowcast Kp data from GFZ
     - 'def' Definitive Kp data from GFZ
     - 'now' Nowcast Kp data from GFZ
     - 'prediction' Predictions from SWPC for the next 3 days
@@ -80,8 +79,7 @@ from pysatSpaceWeather.instruments import methods
 
 platform = 'sw'
 name = 'kp'
-tags = {'': 'Deprecated, mixed definitive and nowcast Kp data from GFZ',
-        'def': 'Definitive Kp data from GFZ',
+tags = {'def': 'Definitive Kp data from GFZ',
         'now': 'Nowcast Kp data from GFZ',
         'prediction': 'SWPC Predictions for the next three days',
         'forecast': 'SWPC Forecast data next (3 days)',
@@ -101,9 +99,6 @@ _test_dates = {'': {'def': dt.datetime(2009, 1, 1),
                     'forecast': today + dt.timedelta(days=1),
                     'prediction': today + dt.timedelta(days=1),
                     'recent': today}}
-
-# Other tags assumed to be True
-_test_download_ci = {'': {'': False}}
 
 # ----------------------------------------------------------------------------
 # Instrument methods
@@ -154,117 +149,10 @@ def load(fnames, tag='', inst_id=''):
     ----
     Called by pysat. Not intended for direct use by user.
 
-    Warnings
-    --------
-    tag '' has been deprecated, will be removed in version 0.2.0+
-
     """
 
     meta = pysat.Meta()
-    if tag == '':
-        # This data type has been deprecated due to changes at GFZ
-        warnings.warn("".join(["Changes at the GFZ database have led to this",
-                               " data type being deprecated. Switch to using",
-                               " 'def' for definitive Kp or 'now' for Kp ",
-                               "nowcasts from GFZ. Load support will be ",
-                               "removed in version 0.2.0+"]),
-                      DeprecationWarning, stacklevel=2)
-
-        # Kp data stored monthly, need to return data daily.  The daily date is
-        # attached to filename.  Parse off the last date, load month of data,
-        # and downselect to the desired day
-        data = pds.DataFrame()
-
-        # Set up fixed width format for these files, only selecting the date
-        # and daily 3-hour Kp values
-        date_slice = slice(0, 6)
-        kp_slice = [slice(7, 10), slice(10, 13), slice(13, 16), slice(16, 19),
-                    slice(19, 23), slice(23, 26), slice(26, 29), slice(29, 32)]
-
-        # These are monthly files, if a date range is desired, test here.
-        # Does not assume an ordered list, but the date range must be continous
-        # within a given month.
-        unique_fnames = dict()
-        for filename in fnames:
-            fname = filename[0:-11]
-            fdate = dt.datetime.strptime(filename[-10:], '%Y-%m-%d')
-            if fname not in unique_fnames.keys():
-                unique_fnames[fname] = [fdate]
-            else:
-                unique_fnames[fname].append(fdate)
-
-        # Load all of the desired filenames
-        all_data = []
-        for fname in unique_fnames.keys():
-            # The daily date is attached to the filename.  Parse off the last
-            # date, load month of data, downselect to the desired day
-            fdate = min(unique_fnames[fname])
-
-            with open(fname, 'r') as fin:
-                temp = fin.readlines()
-
-            if len(temp) == 0:
-                pysat.logger.warn('Empty file: {:}'.format(fname))
-                continue
-
-            # This file has a different format if it is historic or a file that
-            # is being actively updated.  In either case, this line will
-            # remove the appropriate number of summmary lines.
-            ilast = -1 if temp[-1].find('Mean') > 0 else -4
-            temp = np.array(temp[:ilast])
-
-            # Re-format the time data
-            temp_index = np.array([dt.datetime.strptime(tline[date_slice],
-                                                        '%y%m%d')
-                                   for tline in temp])
-
-            idx, = np.where((temp_index >= fdate)
-                            & (temp_index < max(unique_fnames[fname])
-                               + dt.timedelta(days=1)))
-
-            temp_data = list()
-            for tline in temp[idx]:
-                temp_data.append(list())
-                for col in kp_slice:
-                    temp_data[-1].append(tline[col].strip())
-
-            # Select the desired times and add to data list
-            all_data.append(pds.DataFrame(temp_data, index=temp_index[idx]))
-
-        # Combine data together
-        data = pds.concat(all_data, axis=0, sort=True)
-
-        if len(data.index) > 0:
-            # Each column increments UT by three hours. Produce a single data
-            # series that has Kp value monotonically increasing in time with
-            # appropriate datetime indices
-            data_series = pds.Series(dtype='float64')
-            for i in np.arange(8):
-                tind = data.index + pds.DateOffset(hours=int(3 * i))
-                temp = pds.Series(data.iloc[:, i].values, index=tind)
-                data_series = pds.concat([data_series, temp])
-
-            data_series = data_series.sort_index()
-            data_series.index.name = 'time'
-
-            # Kp comes in non-user friendly values like 2-, 2o, and 2+. Relate
-            # these to 1.667, 2.0, 2.333 for processing and user friendliness
-            first = np.array([np.float64(str_val[0])
-                              for str_val in data_series])
-            flag = np.array([str_val[1] for str_val in data_series])
-
-            ind, = np.where(flag == '+')
-            first[ind] += 1.0 / 3.0
-            ind, = np.where(flag == '-')
-            first[ind] -= 1.0 / 3.0
-
-            result = pds.DataFrame(first, columns=['Kp'],
-                                   index=data_series.index)
-        else:
-            result = pds.DataFrame()
-
-        fill_val = np.nan
-    elif tag in ['def', 'now']:
+    if tag in ['def', 'now']:
         # Load the definitive or nowcast data. The Kp data stored in yearly
         # files, and we need to return data daily.  The daily date is
         # attached to filename.  Parse off the last date, load month of data,
@@ -282,7 +170,7 @@ def load(fnames, tag='', inst_id=''):
         fill_val = -1
 
     # Initalize the meta data
-    if tag in ['', 'forecast', 'recent', 'prediction']:
+    if tag in ['forecast', 'recent', 'prediction']:
         for kk in result.keys():
             methods.kp_ap.initialize_kp_metadata(meta, kk, fill_val)
     else:
@@ -319,35 +207,9 @@ def list_files(tag='', inst_id='', data_path='', format_str=None):
     ----
     Called by pysat. Not intended for direct use by user.
 
-    Warnings
-    --------
-    The '' tag has been deprecated and local file listing support will
-    be removed in version 0.2.0+
-
     """
 
-    if tag == '':
-        # This data type has been deprecated due to changes at GFZ
-        warnings.warn("".join(["Changes at the GFZ database have led to this",
-                               " data type being deprecated. Switch to using",
-                               " 'def' for definitive Kp or 'now' for Kp ",
-                               "nowcasts from GFZ. Local file listing support ",
-                               "will be removed in version 0.2.0+"]),
-                      DeprecationWarning, stacklevel=2)
-
-        # Files are by month, going to add date to monthly filename for
-        # each day of the month. The load routine will load a month of
-        # data and use the appended date to select out appropriate data.
-        if format_str is None:
-            format_str = 'kp{year:2d}{month:02d}.tab'
-        files = pysat.Files.from_os(data_path=data_path, format_str=format_str,
-                                    two_digit_year_break=0)
-        if not files.empty:
-            files.loc[files.index[-1] + pds.DateOffset(months=1)
-                      - pds.DateOffset(days=1)] = files.iloc[-1]
-            files = files.asfreq('D', 'pad')
-            files = files + '_' + files.index.strftime('%Y-%m-%d')
-    elif tag in ['def', 'now']:
+    if tag in ['def', 'now']:
         files = methods.gfz.kp_ap_cp_list_files(name, tag, inst_id, data_path,
                                                 format_str=format_str)
     else:
@@ -389,21 +251,10 @@ def download(date_array, tag, inst_id, data_path, mock_download_dir=None):
     --------
     Only able to download current forecast data, not archived forecasts.
 
-    The '' tag has been deprecated and downloads are no longer supported by
-    the source.  Use 'dep' or 'now' instead.
-
     """
 
     # Download standard Kp data
-    if tag == '':
-        # This data type has been deprecated due to changes at GFZ
-        warnings.warn("".join(["Changes at the GFZ database have led to this",
-                               " data type being deprecated. Switch to using",
-                               " 'def' for definitive Kp or 'now' for Kp ",
-                               "nowcasts from GFZ. Downloads are no longer ",
-                               "supported by GFZ."]),
-                      DeprecationWarning, stacklevel=2)
-    elif tag in ['def', 'now']:
+    if tag in ['def', 'now']:
         methods.gfz.kp_ap_cp_download(platform, name, date_array, tag, inst_id,
                                       data_path, mock_download_dir)
     elif tag == 'forecast':
